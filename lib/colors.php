eba1e2a246fd51ebf9dbecd5539e6eae13f13f5c
Jon Baldie <baldiescotty@gmail.com>
2015-09-22 18:51:28 +0100

    Update colors.php

--- lib/colors.php
+++ lib/colors.php
@@ -3,19 +3,19 @@
 $colors = array(
 	'red' => array(
 		'web' => '<font color="#FF0000">',
-		'nix' => "\033[01;31m",
+		'nix' => "\033[01;31m"
 	),
 	'green'	=> array(
 		'web' => '<font color="#00FF00">',
-		'nix' => "\033[01;32m",
+		'nix' => "\033[01;32m"
 	),
 	'yellow' => array(
 		'web' => '<font color="#FFFF00">',
-		'nix' => "\033[01;33m",
+		'nix' => "\033[01;33m"
 	),
 	'clear'	=> array(
 		'web' => '</font>',
-		'nix' => "\033[0m",
+		'nix' => "\033[0m"
 	)
 );
 
